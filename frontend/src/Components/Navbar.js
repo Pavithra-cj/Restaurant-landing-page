2aa70276a95e4c8b3400b1ce1478f88fb082ffcc
Pavithra Chamod J <84024015+Pavithra-cj@users.noreply.github.com>
2023-09-07 20:02:54 +0530

    Navbar done.

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import logo from '../Assets/Images/logoS.png'
 import { useRef } from 'react';
+import { cart } from '../Data';
 
 const Navbar = () => {
 
+    const navbarRef = useRef();
     const searchRef = useRef();
+    const cartRef = useRef();
+
+    const navbarHandler = () => {
+    navbarRef.current.classList.toggle('active');
+    searchRef.current.classList.remove('active');
+    cartRef.current.classList.remove('active');
+    };
 
     const searchHandler = () => {
-        searchRef.current.classList.toggle("active");
+    searchRef.current.classList.toggle('active');
+    navbarRef.current.classList.remove('active');
+    cartRef.current.classList.remove('active');
+     };
+    const cartHandler = () => {
+    cartRef.current.classList.toggle("active");
+    searchRef.current.classList.remove("active");
+    navbarRef.current.classList.remove("active");
     };
 
   return (
@@ -29,13 +45,31 @@ const Navbar = () => {
                 id='search-btn'
                 onClick={searchHandler}></div>
                 <div className='fa fa-shopping-cart'
-                id='cart-btn'></div>
-                <div className='fa fa-bars' id='menu-btn'></div>
+                id='cart-btn'
+                onClick={cartHandler}></div>
+                <div className='fa fa-bars' 
+                id='menu-btn'
+                onClick={navbarHandler}></div>
             </div>
             <div className="search-form" ref={searchRef}>
                 <input type="search" id="search-box" placeholder="Search" />
                 <label htmlFor="search-box" className="fa fa-search"></label>
             </div>
+            <div className="cart-items-container" ref={cartRef}>
+          {cart.map((item, index) => (
+            <div className="cart-item" key={index * Math.random()}>
+              <span className="fas fa-times"></span>
+              <img src={item.img} alt="" />
+              <div className="content">
+                <h3>cart item 01</h3>
+                <div className="price">$15.99/-</div>
+              </div>
+            </div>
+          ))}
+          <a href="#" className="btn">
+            checkout now
+          </a>
+        </div>
         </header>
     </div>
   )
