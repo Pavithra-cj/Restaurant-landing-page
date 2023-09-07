a1ddafc10dc8f18787b3c610ec665cc0c1a28260
Pavithra Chamod J <84024015+Pavithra-cj@users.noreply.github.com>
2023-09-07 21:21:26 +0530

    Footer component done.

---
+++ frontend/src/Components/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <section class="footer">
+        <div class="share">
+          <a href="#" class="fa fa-facebook-f"></a>
+          <a href="#" class="fa fa-twitter"></a>
+          <a href="#" class="fa fa-instagram"></a>
+          <a href="#" class="fa fa-linkedin"></a>
+        </div>
+        <div class="links">
+          <a href="#">home</a>
+          <a href="#">about</a>
+          <a href="#">menu</a>
+          <a href="#">review</a>
+          <a href="#">contact</a>
+        </div>
+        <div class="credit">
+          created by <span>Scorpio</span> | all rights reserved
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Footer
